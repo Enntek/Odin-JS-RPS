574380ebbf485e4639129e4960047fb3b59b6ea7
Enntek <abluecar@gmail.com>
2022-07-08 15:44:57 -0400

    test git branch feature with temp branch

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//this function will contain a random selection: rock, paper, or scissors
+// Select randomly: rock, paper, or scissors
 function computerPlay() {
     let rand = Math.random();
     if (rand < 1/3) {
@@ -14,23 +14,22 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
 
+    // the 3 conditions: win, tie, or loss
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-        (playerSelection == 'paper' && computerSelection == 'rock') || 
-        (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        //win
+            (playerSelection == 'paper' && computerSelection == 'rock') || 
+            (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        
         playerWinCount++;
         console.log(`${playerSelection} versus ${computerSelection}. You win!`);
-
+    
     } else if (playerSelection == computerSelection) {
-        //tie
         console.log(`${playerSelection} versus ${computerSelection}. It's a draw!`); 
 
     } else {
-        //loss (if not a win or a tie, then it's a loss)
         computerWinCount++;
         console.log(`${playerSelection} versus ${computerSelection}. You lose!`); 
     }
-
+    
     console.log(`Player wins: ${playerWinCount}, Computer wins: ${computerWinCount}`);
 }
 
@@ -40,17 +39,19 @@ function game() {
         playerSelection = prompt(`Please type 'rock', 'paper', or 'scissors'.`).toLowerCase();
         computerSelection = computerPlay();
         playRound(playerSelection, computerSelection);
-        // console.log('computerSelection', computerSelection);
     }
+
     console.log(`You played 5 games! 
     You won ${playerWinCount} time(s).
     The computer won ${computerWinCount} time(s).`);
 }
 
-//add a way to use regular expressions to filter out invalid inputs such as numbers or symbols
-//check for valid inputs: 'rock' 'paper' 'scissors'
+
 let playerSelection; 
 let computerSelection; 
 let computerWinCount = 0;
 let playerWinCount = 0;
 game();
+
+// Add: learn regular expression and filter out incorrect inputs, allow 'rock',
+// 'paper', 'scissors' only 
