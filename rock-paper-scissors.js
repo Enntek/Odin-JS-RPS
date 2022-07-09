1a2e14b58eb49d4937d5483a2640429775d07d42
Enntek <abluecar@gmail.com>
2022-07-09 10:29:10 -0400

    Get basic UI working, can play 'single round' game
    The UI is not pretty yet, we will work on that.
    Also we will add player/comp win count,
    a text box stating who won,
    a game end/reset after 5 wins,
    a slight delay after selecting r/p/s,
    keyboard shortcuts to r/p/s.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,60 +1,5 @@
 'use strict';
 
-
-
-const container = document.querySelector('#container');
-
-// create the 3 'buttons', each button is 2 boxes inside of 1 box
-
-const rockBox = document.createElement('div');
-container.appendChild(rockBox);
-
-const rockTop = document.createElement('div');
-rockTop.classList.add('content');
-rockTop.textContent = 'R';
-rockBox.appendChild(rockTop);
-
-const rockBottom = document.createElement('div')
-rockBottom.classList.add('content');
-rockBottom.textContent = 'Rock';
-rockBox.appendChild(rockBottom);
-
-const paperBox = document.createElement('div');
-container.appendChild(paperBox);
-
-const paperTop = document.createElement('div');
-paperTop.classList.add('content');
-paperTop.textContent = 'P';
-paperBox.appendChild(paperTop);
-
-const paperBottom = document.createElement('div');
-paperBottom.classList.add('content');
-paperBottom.textContent = 'Paper';
-paperBox.appendChild(paperBottom);
-
-const scissorsBox = document.createElement('div');
-container.appendChild(scissorsBox)
-
-const scissorsTop = document.createElement('div');
-scissorsTop.classList.add('content');
-scissorsTop.textContent = 'S';
-scissorsBox.appendChild(scissorsTop);
-
-const scissorsBottom = document.createElement('div');
-scissorsBottom.classList.add('content');
-scissorsBottom.textContent = 'Scissors';
-scissorsBox.appendChild(scissorsBottom);
-
-
-
-
-
-
-
-
-
-
-
 // Select randomly: rock, paper, or scissors
 function computerPlay() {
     let rand = Math.random();
@@ -68,6 +13,8 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+
+    computerSelection = computerPlay();
 
     // the 3 conditions: win, tie, or loss
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
@@ -88,10 +35,10 @@ function playRound(playerSelection, computerSelection) {
     console.log(`Player wins: ${playerWinCount}, Computer wins: ${computerWinCount}`);
 }
 
-function game() {
+function game(choice) {
 
     for (let i = 0; i < 5; i++) {
-        playerSelection = prompt(`Please type 'rock', 'paper', or 'scissors'.`).toLowerCase();
+        playerSelection = choice;
         computerSelection = computerPlay();
         playRound(playerSelection, computerSelection);
     }
@@ -106,6 +53,70 @@ let computerSelection;
 let computerWinCount = 0;
 let playerWinCount = 0;
 // game();
+
+
+const container = document.querySelector('#container');
+container.setAttribute('class', 'container1')
+
+const rockBox = document.createElement('div');
+rockBox.setAttribute('class', 'rock')
+container.appendChild(rockBox);
+
+const rockTop = document.createElement('div');
+rockTop.classList.add('content');
+rockTop.textContent = 'R';
+rockBox.appendChild(rockTop);
+
+const rockBottom = document.createElement('div')
+rockBottom.classList.add('content');
+rockBottom.textContent = 'Rock';
+rockBox.appendChild(rockBottom);
+
+const paperBox = document.createElement('div');
+paperBox.setAttribute('class', 'paper')
+container.appendChild(paperBox);
+
+const paperTop = document.createElement('div');
+paperTop.classList.add('content');
+paperTop.textContent = 'P';
+paperBox.appendChild(paperTop);
+
+const paperBottom = document.createElement('div');
+paperBottom.classList.add('content');
+paperBottom.textContent = 'Paper';
+paperBox.appendChild(paperBottom);
+
+const scissorsBox = document.createElement('div');
+scissorsBox.setAttribute('class', 'scissors');
+container.appendChild(scissorsBox)
+
+const scissorsTop = document.createElement('div');
+scissorsTop.classList.add('content');
+scissorsTop.textContent = 'S';
+scissorsBox.appendChild(scissorsTop);
+
+const scissorsBottom = document.createElement('div');
+scissorsBottom.classList.add('content');
+scissorsBottom.textContent = 'Scissors';
+scissorsBox.appendChild(scissorsBottom);
+
+const nodeList = document.querySelectorAll('#container > div')
+
+function logClick(e) {
+    
+    if (this.classList.value == 'rock') {
+        playRound('rock')
+    } else if (this.classList.value == 'paper') {
+        playRound('paper')
+    } else if (this.classList.value == 'scissors') {
+        playRound('scissors')
+    }
+
+    // console.log(e.target.id);
+    // console.log(this.classList.value);
+}
+
+nodeList.forEach((child) => child.addEventListener('click', logClick))
 
 // Add: learn regular expression and filter out incorrect inputs, allow 'rock',
 // 'paper', 'scissors' only 
