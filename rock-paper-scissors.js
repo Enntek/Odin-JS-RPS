42c16d2bd8ea8823943410bacac81730dd70a7cb
Enntek <abluecar@gmail.com>
2022-07-10 20:01:04 -0400

    Tweak UI, remove key press functionality
    I tried to add a transition but it's beyond my abilities.
    I will tweak key press and add it back in.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,6 +2,7 @@
 // add transition (grow box)
 // add sound
 // add countdown of 3 seconds per game
+// implement data-code like in tutorial
 
 'use strict';
 document.body.style.backgroundImage = "url('white-bg.jpg')"
@@ -46,12 +47,12 @@ function playRound(playerSelection, computerSelection) {
     textBox.textContent = `Player wins: ${playerWinCount} 
     Computer wins: ${computerWinCount}`;
 
-    if (playerWinCount == 3) {
+    if (playerWinCount == 5) {
         playerWinCount = 0;
         computerWinCount = 0;
-        textBox.textContent = `Player wins. Play again?`;
+        textBox.textContent = `Player wins! Play again?`;
         
-    } else if (computerWinCount == 3) {
+    } else if (computerWinCount == 5) {
         playerWinCount = 0;
         computerWinCount = 0;
         textBox.textContent = `Computer wins. Play again?`; 
@@ -89,11 +90,14 @@ textBox.textContent = 'Select rock, paper or scissors to begin game:'
 container.appendChild(textBox);
 
 const buttonBox = document.createElement('div');
+buttonBox.setAttribute('class', 'cbuttonBox');
 buttonBox.setAttribute('id', 'buttonBox');
 container.appendChild(buttonBox);
 
 const rockBox = document.createElement('div');
-rockBox.setAttribute('class', 'rock')
+rockBox.setAttribute('class', 'choice')
+rockBox.setAttribute('id', 'rockID')
+rockBox.setAttribute('data-code', 'KeyR')
 buttonBox.appendChild(rockBox);
 
 const rockTop = document.createElement('div');
@@ -107,7 +111,9 @@ rockBottom.textContent = 'Rock';
 rockBox.appendChild(rockBottom);
 
 const paperBox = document.createElement('div');
-paperBox.setAttribute('class', 'paper')
+paperBox.setAttribute('class', 'choice')
+paperBox.setAttribute('id', 'paperID')
+paperBox.setAttribute('data-code', 'KeyP')
 buttonBox.appendChild(paperBox);
 
 const paperTop = document.createElement('div');
@@ -121,7 +127,9 @@ paperBottom.textContent = 'Paper';
 paperBox.appendChild(paperBottom);
 
 const scissorsBox = document.createElement('div');
-scissorsBox.setAttribute('class', 'scissors');
+scissorsBox.setAttribute('class', 'choice');
+scissorsBox.setAttribute('id', 'scissorsID')
+scissorsBox.setAttribute('data-code', 'KeyS');
 buttonBox.appendChild(scissorsBox)
 
 const scissorsTop = document.createElement('div');
@@ -138,33 +146,55 @@ const nodeList = document.querySelectorAll('#buttonBox > div')
 
 function logClick(e) {
     
-    if (this.classList.value == 'rock') {
+    if (this.id == 'rockID') {
         playRound('rock')
-    } else if (this.classList.value == 'paper') {
+    } else if (this.id == 'paperID') {
         playRound('paper')
-    } else if (this.classList.value == 'scissors') {
+    } else if (this.id == 'scissorsID') {
         playRound('scissors')
     }
 
+    //else if (this.classList.value == 'scissors')
     // console.log(e.target.id);
+    // console.log(e.target.className); 
     // console.log(this.classList.value);
 }
 
 nodeList.forEach((child) => child.addEventListener('click', logClick))
 
-function logKey(e) {
-    if (e.code == 'KeyR') {
-        playRound('rock')
-    } else if (e.code == 'KeyP') {
-        playRound('paper')
-    } else if (e.code == 'KeyS') {
-        playRound('scissors')
-    }
-    // console.log(e.code);
-    // console.log(e.key);
-}
+// function logKey(e) {
+//     if (e.code == 'KeyR') {
+//         document.querySelector('#rockID').classList.add('playing')
+//         playRound('rock')
+//     } else if (e.code == 'KeyP') {
+//         document.querySelector('#paperID').classList.add('playing')
+//         playRound('paper')
+//     } else if (e.code == 'KeyS') {
+//         document.querySelector('#scissorsID').classList.add('playing')
+//         playRound('scissors')
+//     }
 
-window.addEventListener('keydown', logKey)
+// }
+
+// window.addEventListener('keydown', logKey)
+
+// function removeTransition(e) {
+//     if (e.code == 'KeyR') {
+//         document.querySelector('#rockID').classList.remove('playing')
+//         playRound('rock')
+//     } else if (e.code == 'KeyP') {
+//         document.querySelector('#paperID').classList.remove('playing')
+//         playRound('paper')
+//     } else if (e.code == 'KeyS') {
+//         document.querySelector('#scissorsID').classList.remove('playing')
+//         playRound('scissors')
+//     }
+//     // this.classList.remove('playing')
+// }
+
+// window.addEventListener('keyup', removeTransition)
+
+
 
 // Add: learn regular expression and filter out incorrect inputs, allow 'rock',
 // 'paper', 'scissors' only 
